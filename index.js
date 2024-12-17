e8da63defc5a8291e55db54cc5b2d90842d00143
niraj bawiskar <nirajbawiskar77@gmail.com>
2024-12-17 09:58:21 +0530

    helo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("dotenv").config()
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 const cookieParser = require("cookie-parser")
 const { adminProtected, customerProtected } = require("./middlewares/protected.middleware")
 
@@ -12,6 +13,7 @@ app.use(cors({
 }))
 app.use(express.json()) //body parse
 app.use(cookieParser()) //body parse
+app.use(express.static(dist)) //body parse
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
@@ -19,12 +21,11 @@ app.use("/api/public", require("./routes/public.routes"))
 app.use("/api/customer", customerProtected, require("./routes/customer.routes"))
 
 app.use("*", (req,res) => {
-    res.status(404).json({message:"resource not found"})
+   res.sendFile(path.join(__dirname,"dist","index.html"))
 })
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.once("open", () => {
     console.log("mongo connected");
+    app.listen(process.env.PORT, console.log("server running"))
 
 })
-
-app.listen(process.env.PORT, console.log("server running"))
