6a56fc907b5c404fc5109610aa96cff2c82d0b62
niraj bawiskar <nirajbawiskar77@gmail.com>
2024-12-17 10:16:02 +0530

    niraj

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 }))
 app.use(express.json()) //body parse
 app.use(cookieParser()) //body parse
-app.use(express.static(dist)) //body parse
+app.use(express.static("dist")) //body parse
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/auth", require("./routes/auth.routes"))
